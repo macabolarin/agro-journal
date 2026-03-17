d320f6ae27487106b5316026642d3ca30009785a
macabolarin <macabolarin@gmail.com>
2026-03-17 01:03:53 +0100

    add recent file

---
+++ V-CLASS/vclass.js
@@ -0,0 +1,6 @@
+<select id="my-dropdown" onchange="redirect(this)">
+    <option value="">basic 6</option>
+    <option value="https:
+    <option value="https></option>
+    <option value="https:basic 6,html">basic 6</option>
+</select>
